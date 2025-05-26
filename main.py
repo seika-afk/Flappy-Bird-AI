cdc30ef331f08ff8f8654566662124726966ab71
seika-afk <artistnoob6@gmail.com>
2025-05-26 00:37:12 +0000

    25th may

---
+++ main.py
@@ -0,0 +1,49 @@
+
+import pygame
+from sys import exit
+import config
+import components
+import population
+
+
+pygame.init()
+population=population.Population(100)
+clock=pygame.time.Clock()
+
+
+def generate_pipes():
+    config.pipes.append(components.Pipes(config.win_width))
+
+def quit_game():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+def main():
+    pipes_spawn_time=10
+    while True:
+        quit_game()
+
+        config.window.fill((0,0,0))
+        config.ground.draw(config.window)
+
+        if pipes_spawn_time <=0:
+            generate_pipes()
+            pipes_spawn_time=200
+        pipes_spawn_time-=1
+
+        for p in config.pipes:
+            p.draw(config.window)
+            p.update()
+            if(p.off_screen):
+                config.pipes.remove(p)
+        if(not population.extinct()):
+            population.update_live_players()
+        else:
+            pass
+
+        clock.tick(60)
+        pygame.display.flip()
+
+main()
---
+++ population.py
@@ -0,0 +1,27 @@
+import config
+
+import player
+
+class Population:
+    def __init__(self,size):
+        self.players=[]
+        self.size=size
+
+        for i in range(0,self.size):
+            self.players.append(player.Player())
+
+
+
+    def update_live_players(self):
+        for p in self.players:
+            if p.alive:
+               p.look()
+               p.think()
+               p.draw(config.window)
+               p.update(config.ground)
+    def extinct(self):
+        extinct=True
+        for p in self.players:
+            if p.alive:
+                extinct=False
+        return extinct
